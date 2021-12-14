f770deff5f3b72b3c7875fca9cef69d475b3b66e
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2021-12-15 00:38:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,11 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
+# Modify X86 Kernel 5.10
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 # Clear the login password
-# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加 smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
