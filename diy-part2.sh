fec66b9abcebabe7babca39e3e95ddf683e27fcb
mitsukileung <38887306+mitsukileung@users.noreply.github.com>
2021-12-14 12:04:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,20 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.88/g' package/base-files/files/bin/config_generate
+
+# Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+# 添加 smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
+# 添加 adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# 添加 filebrowser
+git clone https://github.com/takayukileung/luci-app-filebrowser.git package/luci-app-filebrowser
+
+# 添加 OpenClash
+git clone -b master https://github.com/vernesong/OpenClash.git package/openclash
